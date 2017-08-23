db4c16efc2f4f5a11928b589660caa291c0871b5
Dariush <lotfi.developer@live.com>
2017-08-23 16:29:01 +0430

    Fix AdamAccumulate by Reza, Change UNet to accept other activations

--- models/u_net.py
+++ models/u_net.py
@@ -1,32 +1,36 @@
 from keras.models import Model
-from keras.layers import Input, Concatenate, Conv2D, MaxPooling2D, Conv2DTranspose, UpSampling2D, BatchNormalization, Dropout
+from keras.layers import Input, Concatenate, Conv2D, MaxPooling2D, Conv2DTranspose, UpSampling2D, BatchNormalization, Dropout, Activation
 
-def conv_block(inputs, filters, acti, init, bn, do=0):
-    n = Conv2D(filters, (3, 3), activation=acti, kernel_initializer=init, padding='same')(inputs)
+def conv_block(inputs, filters, acti_layer, init, bn, do=0):
+    n = Conv2D(filters, (3, 3), kernel_initializer=init, padding='same')(inputs)
+    n = acti_layer(n)
     n = BatchNormalization()(n) if bn else n
     n = Dropout(do)(n) if do else n
-    n = Conv2D(filters, (3, 3), activation=acti, kernel_initializer=init, padding='same')(n)
+    n = Conv2D(filters, (3, 3), kernel_initializer=init, padding='same')(n)
+    n = acti_layer(n)
     n = BatchNormalization()(n) if bn else n
     return n
 
-def level_block(inputs, filters, depth, acti, init, do, bn, mp, up):
+def level_block(inputs, filters, depth, acti_layer, init, do, dob, bn, mp, up):
     if depth > 0:
-        n = conv_block(inputs, filters, acti, init, bn, do)
+        n = conv_block(inputs, filters, acti_layer, init, bn, 0 if dob else do)
         m = MaxPooling2D()(n) if mp else Conv2D(filters, (3, 3), strides=(2, 2), padding='same')(n)
-        m = level_block(m, 2*filters, depth-1, acti, init, do, bn, mp, up)
+        m = level_block(m, 2*filters, depth-1, acti_layer, init, do, dob, bn, mp, up)
         if up:
             m = UpSampling2D()(m)
-            m = Conv2D(filters, (2, 2), activation=acti, kernel_initializer=init, padding='same')(m)
+            m = Conv2D(filters, (2, 2), kernel_initializer=init, padding='same')(m)
+            m = acti_layer(m)
         else:
-            m = Conv2DTranspose(filters, (3, 3), strides=(2, 2), activation=acti, kernel_initializer=init, padding='same')(m)
+            m = Conv2DTranspose(filters, (3, 3), strides=(2, 2), kernel_initializer=init, padding='same')(m)
+            m = acti_layer(m)
         m = Concatenate()([n, m])
-        m = conv_block(m, filters, acti, init, bn)
+        m = conv_block(m, filters, acti_layer, init, bn)
     else:
-        m = conv_block(inputs, filters, acti, init, bn, do)
+        m = conv_block(inputs, filters, acti_layer, init, bn, do)
     return m
 
-def UNet(img_shape, num_classes=1, filters=64, depth=4, activation='relu', init='he_uniform', 
-         dropout=0.2, batchnorm=True, maxpool=True, upconv=True):
+def UNet(img_shape, num_classes=1, filters=64, depth=4, activation=lambda x: Activation('relu')(x),
+         init='glorot_uniform', dropout=0.2, dropout_base_only=False, batchnorm=True, maxpool=True, upconv=True):
     '''
     U-Net: Convolutional Networks for Biomedical Image Segmentation
     (https://arxiv.org/abs/1505.04597)
@@ -35,7 +39,7 @@ def UNet(img_shape, num_classes=1, filters=64, depth=4, activation='relu', init=
     num_classes: number of output channels
     filters: number of filters of the first conv
     depth: zero indexed depth of the U-structure
-    activation: activation function after convolutions
+    activation: activation layer function after convolutions    default: Relu
     init: kernels initialization function
     dropout: amount of dropout in the contracting part
     batchnorm: adds Batch Normalization if true
@@ -43,6 +47,6 @@ def UNet(img_shape, num_classes=1, filters=64, depth=4, activation='relu', init=
     upconv: use transposed conv instead of upsamping + conv if false
     '''
     i = Input(shape=img_shape)
-    o = level_block(i, filters, depth, activation, init, dropout, batchnorm, maxpool, upconv)
+    o = level_block(i, filters, depth, activation, init, dropout, dropout_base_only, batchnorm, maxpool, upconv)
     o = Conv2D(num_classes, (1, 1), activation='sigmoid')(o)
     return Model(inputs=i, outputs=o)
